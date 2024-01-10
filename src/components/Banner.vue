82304887574ddf452165fed42b4fcc28f5fae8c4
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-01-10 12:52:54 -0600

    Banner info

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,13 +5,10 @@
       <div class="row">
         <div class="col-md-8 col-sm-12">
           <div class="banner-text">
-            <h2 class="white">Best App Website Template</h2>
-            <h6 class="white">This awesome template designed by <a href="http://w3Template.com" target="_blank" rel="dofollow" class="weblink">W3 Template</a>.</h6>
-            <p class="banner-text white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur hendrerit neque massa, sit amet tristique ante porta ut. In sodales et justo vel vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-            <ul>
-              <li><a href="#"><img src="/frontend/images/appstore.png" class="wow fadeInUp" data-wow-delay="0.4s"/></a></li>
-              <li><a href="#"><img src="/frontend/images/playstore.png" class="wow fadeInUp" data-wow-delay="0.7s"/></a></li>
-            </ul>
+            <h2 class="white">Conectamos mujeres que quieren hacer crecer su carrera en tecnología💜</h2>
+            <p class="banner-text white">Sé parte de nuestra exclusiva comunidad de mujeres y ten acceso a contenido de tecnología, vacantes, mentoria, networking y eventos. <br>
+              <b>¡Hecho por mujeres, para mujeres!</b></p>
+            <a href="https://airtable.com/app90D1HDCxRMwMBz/shrwanQZlOV27jsDn" class="about-btn">Regístrate gratis aquí</a>
           </div>
         </div>
         <div class="col-md-4 col-sm-12"> <img src="/frontend/images/iphone-screen.png" class="img-fluid wow fadeInUp"/> </div>
